99b6eeae367473c30229b5fedf8769315beb963b
mattbedford <matt.bedford@gmail.com>
2023-10-03 08:08:18 +0200

    switched to vue stack with vite

---
+++ Qrcodes/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
